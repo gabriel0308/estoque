59d30ee312ca096dada299ab0ed4c1d8410a28df
Gabriel Schwind Santos <CS261967@cosan.rede>
2018-05-24 14:49:38 -0300

    Correção Metodo de Autenticação

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -44,23 +44,22 @@ class LoginController extends Controller
 
 
        $analistaController = new AnalistaController();
-        
-       $credentials = $request->only('MatriculaAnalista','password');
-       $credentials['password'] = sha1($credentials['password']);
            
-       //$logins = $analistaController->search($request->MatriculaAnalista, sha1($request->SenhaAnalista));
-       if(Auth::attempt(array('MatriculaAnalista' => $credentials['MatriculaAnalista'], 'password' => $credentials['password']), 1))
-       echo("funciona"); 
+       $logins = $analistaController->search($request->MatriculaAnalista, $request->password);
        
-       // if($logins != null)
-        // {
-        //     $request->session()->put('analistaNome', $logins["NomeAnalista"]);
-        //     $request->session()->put('analistaMatricula', $logins["MatriculaAnalista"]);
-        //     //echo($request->session()->get('analistaNome').'<br>'.$request->session()->get('analistaMatricula'));
+       if($logins != null)
+        {
+            // $request->session()->put('analistaNome', $logins["NomeAnalista"]);
+            // $request->session()->put('analistaMatricula', $logins["MatriculaAnalista"]);
+            // //echo($request->session()->get('analistaNome').'<br>'.$request->session()->get('analistaMatricula'));
 
-        //     Auth::guard();
-        //     return view('home')->with('analistaNome', $logins["NomeAnalista"])->with('analistaMatricula', $logins["MatriculaAnalista"]);
-        // }
+            // Auth::guard();
+           
+            Auth::loginUsingId($logins->IdAnalista); 
+            //->loginUsingId($logins->idAnalista);
+            return view('home')->with('analistaNome', $logins["NomeAnalista"])->with('analistaMatricula', $logins["MatriculaAnalista"]);
+
+        }
         
         
         else {
